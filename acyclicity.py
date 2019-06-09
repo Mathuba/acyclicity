66870a9e5f745909175e83abbc15b2cbf2511983
Mathuba Dlamini <Mathuba58@gmail.com>
2019-06-09 14:25:38 +0200

    Added function to build graph and test case

--- acyclicity.py
+++ acyclicity.py
@@ -3,16 +3,21 @@
 import sys
 
 
+def add_edge(graph, vertex1, vertex2):
+    if vertex1 in graph:
+        graph[vertex1].append(vertex2)
+    else:
+        graph[vertex1] = [vertex2]
+
 def acyclic(adj):
     return 0
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-    print(acyclic(adj))
+    n, m = map(int, sys.stdin.readline().split())
+    graph = {vertex: [] for vertex in range(1, n+1)}
+
+    for _ in range(m):
+        vert1, vert2 = map(int, sys.stdin.readline().split())
+        add_edge(graph, vert1, vert2)
+    
+    print(graph)
